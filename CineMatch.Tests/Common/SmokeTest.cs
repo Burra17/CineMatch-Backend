83b4a6277af82b58d3fec08a8416b1d427b74d1c
Joakim Rehn <jocke.94@hotmail.se>
2026-05-07 14:03:34 +0200

    added a test project to test the pipeline.

---
+++ CineMatch.Tests/Common/SmokeTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CineMatch.Tests.Common
+{
+    [TestFixture]
+    public class SmokeTest
+    {
+        [Test]
+        public void TestPipeline_IsConfiguredCorrectly()
+        {
+            // Arrange
+            var expected = true;
+            // Act
+            var actual = true;
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
