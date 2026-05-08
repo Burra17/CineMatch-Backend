f585de8654f862cd2ecfc1ded7f7d6642bec25fd
MrBig83 <mjohansson176@gmail.com>
2026-05-08 15:52:14 +0200

    Add RegisterUser command, handler, and validator

---
+++ src/CineMatch.Application/Features/Users/Commands/RegisterUser/RegisterUserCommandHandler.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using CineMatch.Application.Features.Users.Common.Dtos;
+using CineMatch.Application.Features.Users.Common.Errors;
+using CineMatch.Application.Interfaces;
+using CineMatch.Domain.Enums;
+using CineMatch.Domain.Models;
+using ErrorOr;
+using MediatR;
+
+namespace CineMatch.Application.Features.Users.Commands.RegisterUser
+{
+    public class RegisterUserCommandHandler : IRequestHandler<RegisterUserCommand, ErrorOr<UserDto>>
+    {
+        private readonly IMapper _mapper;
+        private readonly IUserRepository _userRepository;
+        private readonly IPasswordHasher _passwordHasher;
+        private readonly IUnitOfWork _unitOfWork;
+        public RegisterUserCommandHandler(IMapper mapper, IUserRepository userRepository, IPasswordHasher passwordHasher, IUnitOfWork unitOfWork)
+        {
+            _mapper = mapper;
+            _userRepository = userRepository;
+            _passwordHasher = passwordHasher;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<ErrorOr<UserDto>> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
+        {
+            if (await _userRepository.ExistsByEmailAsync(request.Email, cancellationToken)) 
+            {
+                return UserErrors.EmailAlreadyExists;   
+            }
+            
+            if (await _userRepository.ExistsByUsernameAsync(request.Username, cancellationToken))
+            {
+                return UserErrors.UsernameAlreadyExists;
+            }
+
+            var passwordHash = _passwordHasher.HashPassword(request.Password);
+
+            var user = new User
+            {
+                Id = Guid.NewGuid(),
+                Username = request.Username,
+                Email = request.Email,
+                PasswordHash = passwordHash,
+                Role = UserRole.User,
+                IsEmailConfirmed = true,
+                CreatedAt = DateTime.UtcNow
+
+            };
+
+            await _userRepository.AddAsync(user);
+            await _unitOfWork.SaveChangesAsync();
+
+            return _mapper.Map<UserDto>(user);  
+        }
+    }
+}
