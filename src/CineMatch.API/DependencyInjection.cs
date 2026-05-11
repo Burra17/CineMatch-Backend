6c87eac99bc0cbbcc7df6f89c513afeebf7908b1
André Pettersson <andre20030417@gmail.com>
2026-05-11 18:18:24 +0200

    Add GetCurrentUser query and /api/users/me endpoint

---
+++ src/CineMatch.API/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using CineMatch.API.Services;
+using CineMatch.Application.Interfaces;
+
+namespace CineMatch.API
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApiServices(this IServiceCollection services)
+        {
+            services.AddHttpContextAccessor();
+            services.AddScoped<ICurrentUserService, CurrentUserService>();
+            return services;
+        }
+    }
+}
---
+++ src/CineMatch.API/Services/CurrentUserService.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Claims;
+using CineMatch.Application.Interfaces;
+
+namespace CineMatch.API.Services;
+
+public class CurrentUserService : ICurrentUserService
+{
+    private readonly IHttpContextAccessor _contextAccessor;
+
+    public CurrentUserService(IHttpContextAccessor contextAccessor)
+    {
+        _contextAccessor = contextAccessor;
+    }
+
+    public Guid? UserId
+    {
+        get
+        {
+            var userIdClaim = _contextAccessor.HttpContext?.User
+                .FindFirstValue(ClaimTypes.NameIdentifier)
+                ?? _contextAccessor.HttpContext?.User
+                .FindFirstValue("sub");
+
+            return Guid.TryParse(userIdClaim, out var userId) ? userId : null;
+        }
+    }
+
+    public string? Email =>
+        _contextAccessor.HttpContext?.User.FindFirstValue(ClaimTypes.Email)
+        ?? _contextAccessor.HttpContext?.User.FindFirstValue("email");
+}
