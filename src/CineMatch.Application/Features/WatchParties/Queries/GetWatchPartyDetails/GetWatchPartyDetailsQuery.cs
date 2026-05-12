648e07383b7f79484dee39ef323634c6ee5c1c7d
André Pettersson <andre20030417@gmail.com>
2026-05-12 16:27:27 +0200

    add leavewatchpartycommand and getwatchpartydetailsquery

---
+++ src/CineMatch.Application/Features/WatchParties/Queries/GetWatchPartyDetails/GetWatchPartyDetailsQuery.cs
@@ -0,0 +1,7 @@
+﻿using CineMatch.Application.Features.WatchParties.Common.Dtos;
+using ErrorOr;
+using MediatR;
+
+namespace CineMatch.Application.Features.WatchParties.Queries.GetWatchPartyDetails;
+
+public record GetWatchPartyDetailsQuery(Guid WatchPartyId) : IRequest<ErrorOr<WatchPartyDetailsDto>>;
